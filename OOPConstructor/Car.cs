d06631c5a1f6a3563b63861910c58e87035aaf7f
MykolaDekusar <ndekusar97@gmail.com>
2025-07-29 15:30:05 +0200

    Created a Car object with a private model string

---
+++ OOPConstructor/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConstructor
+{
+    internal class Car
+    {
+        private string _modello = "";
+        public Car(string modello) {
+            _modello = modello;
+            Console.WriteLine("Ho creato una macchina con il modello: "+ _modello);
+        }
+    }
+}
