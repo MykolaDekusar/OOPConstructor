d7a66c99c5899e83c3e89176a3ec0bfc999f6818
MykolaDekusar <ndekusar97@gmail.com>
2025-07-30 16:21:49 +0200

    Added Customer class with Name, Address, Number getters and setters and 3 custom constructors

---
+++ OOPConstructor/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConstructor
+{
+    internal class Customer
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Number { get; set; }
+
+        public Customer(string name, string address, string number) {
+            Name = name;
+            Address = address;  
+            Number = number;
+            Console.WriteLine($"Aggiunto cliente {Name} con indirizzo {Address} e telefono {Number}");
+            
+        }
+
+        public Customer(string name, string address)
+        {
+            Name = name;
+            Address = address;
+            Console.WriteLine($"Aggiunto cliente {Name} con indirizzo {Address} e telefono sconosciuto");
+        }
+        public Customer(string name)
+        {
+            Name = name;
+            Console.WriteLine($"Aggiunto cliente {Name} con indirizzo sconosciuto e telefono sconosciuto");
+        }
+    }
+}
